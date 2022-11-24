b3a3ad01cd6301a1b1ad955482c1859e1282a305
basakk123 <basakk@naver.com>
2022-11-24 09:37:28 +0900

    커스텀 익셉션 만들기

--- src/main/java/shop/mtcoding/bank/config/auth/LoginUserService.java
+++ src/main/java/shop/mtcoding/bank/config/auth/LoginUserService.java
@@ -1,7 +1,6 @@
 package shop.mtcoding.bank.config.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
